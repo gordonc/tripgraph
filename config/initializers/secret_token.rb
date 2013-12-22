55c1c9734841add4990e1a20f95dfbed1a425945
gordonc <gchan@fastmail.fm>
2013-12-22 18:06:46 -0500

    add rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tripgraph::Application.config.secret_key_base = '634c9b7115e21cbbed1a327c69ecb0adf0a6f29417a8f01bab0739803a3cd088d4f784dbe95e00181a6822d3df671a4274e833d6fde6c029d8b071063a05d03a'
