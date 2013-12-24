b1c7977387ce0db209da4cc70ad779a0d013d0f5
gordonc <gchan@fastmail.fm>
2013-12-23 23:31:10 -0500

    config cache redis namespace

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Tripgraph
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.cache_store = :redis_store
+    config.cache_store = :redis_store, { :namespace => "cache" }
   end
 end
