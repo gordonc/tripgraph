f2dbcabc2648dcc1d32fb9565ed45ec5a596608f
gordonc <gchan@fastmail.fm>
2013-12-26 21:54:31 -0500

    add gadventures place line regex strip

--- app/workers/g_adventures_trip_parser.rb
+++ app/workers/g_adventures_trip_parser.rb
@@ -20,8 +20,17 @@ class GAdventuresTripParser
     end
 
     doc.css("div#trip-itinerary div#itinerary-brief div.content h5").each do |h5|
-      puts h5.content
+      puts strip_place_line(h5.content)
     end
 
+  end
+
+  def strip_place_line(line)
+      place = line
+      # strip days prefix
+      place = place.sub(%r{^\s*Days?\s+\d+\s+}, '')
+      # strip meals suffix
+      place = place.sub(%r{\s+\((\d+[BLD]\,?)+\)\s*}, '')
+      return place
   end
 end
