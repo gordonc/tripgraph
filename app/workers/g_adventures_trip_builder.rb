bfb7906e80af59f0e81addb81f10ccd95b7fa0b6
gordonc <gchan@fastmail.fm>
2013-12-31 00:54:35 -0500

    add gadventures trip places geocoding

--- app/workers/g_adventures_trip_builder.rb
+++ app/workers/g_adventures_trip_builder.rb
@@ -2,7 +2,22 @@ class GAdventuresTripBuilder
   include Sidekiq::Worker
   def perform(parse_result)
 
-    puts parse_result
+    require 'geocoder'
+
+    name = parse_result['name']
+    places = parse_result['places']
+    regions = parse_result['regions']
+
+    if regions.length > 0
+      country = regions[0]
+      cc_tld = Geocoder.get_cc_tld(country)
+      
+      places.each do |place|
+        puts Geocoder.get_position(place, cc_tld)
+      end
+    else
+      raise "empty regions parse result for trip #{name}"
+    end
 
   end
 end
