61ef4e084255ab4ef141d425f55738de0670cd0c
gordonc <gchan@fastmail.fm>
2014-01-01 23:16:32 -0500

    save trip, place, trip_segment ActiveRecords

--- app/workers/g_adventures_trip_builder.rb
+++ app/workers/g_adventures_trip_builder.rb
@@ -8,12 +8,25 @@ class GAdventuresTripBuilder
     places = parse_result['places']
     regions = parse_result['regions']
 
+    trip = Trip.new({:name => name})
+    trip.save
+
     if regions.length > 0
       country = regions[0]
       cc_tld = Geocoder.get_cc_tld(country)
       
+      prev_place = nil
       places.each do |place|
-        puts Geocoder.get_position(place, cc_tld)
+        position = Geocoder.get_position(place, cc_tld)
+        place = Place.new({:name => place, :lat => position['lat'], :lon => position['lon']})
+        place.save
+
+        unless prev_place.nil?
+          trip_segment = TripSegment.new({:trip => trip, :from_place => prev_place, :to_place => place})
+          trip_segment.save
+        end
+
+        prev_place = place
       end
     else
       raise "empty regions parse result for trip #{name}"
