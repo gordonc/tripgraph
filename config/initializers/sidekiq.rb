72af4a5aae7c1cb2ae77e8ceccaa91e94f3eff3e
gordonc <gchan@fastmail.fm>
2014-01-02 23:56:38 -0500

    disable sidekiq worker retry

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
   config.redis = { :url => 'redis://localhost', :namespace => 'sidekiq' }
+  config.default_worker_options = { 'retry' => false }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { :url => 'redis://localhost', :namespace => 'sidekiq' }
+  config.default_worker_options = { 'retry' => false }
 end
