7a0841d4c90c16e71189a308495400096e6413e0
gordonc <gchan@fastmail.fm>
2014-01-03 23:01:08 -0500

    dedupe trip, place, trip_segment ActiveRecord creation

--- app/workers/trip_writer.rb
+++ app/workers/trip_writer.rb
@@ -6,17 +6,14 @@ class TripWriter
     name = trip['name']
     places = trip['places']
 
-    trip = Trip.new({:url => url, :name => name})
-    trip.save
+    trip = Trip.find_or_create_by({:url => url, :name => name})
 
     prev_place = nil
     places.each do |place|
-      place = Place.new({:name => place['name'], :lat => place['lat'], :lon => place['lon']})
-      place.save
+      place = Place.find_or_create_by({:name => place['name'], :lat => place['lat'], :lon => place['lon']})
 
       unless prev_place.nil?
-        trip_segment = TripSegment.new({:trip => trip, :from_place => prev_place, :to_place => place})
-        trip_segment.save
+        trip_segment = TripSegment.find_or_create_by({:trip => trip, :from_place => prev_place, :to_place => place})
       end
 
       prev_place = place
