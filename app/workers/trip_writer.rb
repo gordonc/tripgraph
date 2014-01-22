44510fe2c540333b893453e51c7414dd000e5185
gordonc <gchan@fastmail.fm>
2014-01-21 22:02:50 -0500

    OCD

--- app/workers/trip_writer.rb
+++ app/workers/trip_writer.rb
@@ -2,11 +2,8 @@ class TripWriter
   include Sidekiq::Worker
   def perform(trip)
 
-    url = trip['url']
-    name = trip['name']
     places = trip['places']
-
-    trip = Trip.find_or_create_by({:url => url, :name => name})
+    trip = Trip.find_or_create_by({:name => trip['name'], :url => trip['url']})
 
     places.each_with_index do |place, i|
       place = Place.find_or_create_by({:name => place['name'], :lat => place['lat'], :lon => place['lon']})
