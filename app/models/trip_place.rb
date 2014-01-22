217133e2997915f9ee521d2ba9d941c005aaf9b0
gordonc <gchan@fastmail.fm>
2014-01-21 22:03:29 -0500

    index child documents for trip and place with trip_place ActiveRecord after_create callback

--- app/models/trip_place.rb
+++ app/models/trip_place.rb
@@ -1,4 +1,60 @@
 class TripPlace < ActiveRecord::Base
   belongs_to :trip
   belongs_to :place
+
+  after_create :index
+
+  @@es = Elasticsearch::Client.new
+
+  @@es.indices.put_mapping index: 'trip',
+                           type: 'places',
+                           body: {
+                             places: {
+                               _parent: {
+                                 type: "trips"
+                               },
+                               properties: {
+                                 location: {
+                                   type: 'geo_point'
+                                 }
+                               }
+                             }
+                           }
+
+  @@es.indices.put_mapping index: 'place',
+                           type: 'trips',
+                           body: {
+                             trips: {
+                               _parent: {
+                                 type: "places"
+                               }
+                             }
+                           }
+
+  def index 
+    @@es.index index: 'trip',
+               type: 'places',
+               parent: self.trip.id,
+               id: self.place.id,
+               body: {
+                 places: {
+                   name: self.place.name,
+                   location: {
+                     lat: self.place.lat,
+                     lon: self.place.lon
+                   }
+                 }
+               }
+
+    @@es.index index: 'place',
+               type: 'trips',
+               parent: self.place.id,
+               id: self.trip.id,
+               body: {
+                 trips: {
+                   name: self.trip.name,
+                   name: self.trip.url
+                 }
+               }
+  end
 end
