ba1957cba589e0e5f32ef193f1c0e68e3fd36f2c
gordonc <gchan@fastmail.fm>
2014-02-01 21:19:13 -0500

    handle gadventures regex and geocoding parse errors

--- app/workers/gadventures_trip_parser.rb
+++ app/workers/gadventures_trip_parser.rb
@@ -17,34 +17,44 @@ class GadventuresTripParser
     begin 
       trip_name = doc.css("meta").select{|meta| meta['property'] == "og\:title"}[0]['content']
     rescue => e
-      raise GadventuresParseError.new("error parsing doc for trip_name", e)
-    end
-    begin
-      place_names = doc.css("div#trip-itinerary>div#itinerary-brief>div.content>h5").collect{|h5| get_places_from_itinerary(h5.content)}.flatten
-    rescue => e
-      raise GadventuresParseError.new("error parsing doc for place_names", e)
+      raise GadventuresParseError.new("error parsing doc for trip name", e)
     end
     begin
       country_names = doc.css("div#trip-itinerary>div.summary>div.content>ul>li").collect{|li| li.content}
     rescue => e
-      raise GadventuresParseError.new("error parsing doc for country_names", e)
+      raise GadventuresParseError.new("error parsing doc for country names", e)
     end
-    places = []
+    begin
+      itinerary_lines = doc.css("div#trip-itinerary>div#itinerary-brief>div.content>h5").collect{|h5| h5.content}
+    rescue => e
+      raise GadventuresParseError.new("error parsing doc for itinerary lines", e)
+    end
 
     cc_tld = get_cc_tld(country_names)
+    place_names = itinerary_lines.collect{|line| get_places_from_itinerary_line(line)}.flatten
+
+    places = []
     place_names.each do |place_name|
-      position = GoogleGeocoder.get_position(place_name, cc_tld)
-      places << {:name => place_name, :lat => position.lat, :lon => position.lon}
+      begin
+        position = GoogleGeocoder.get_position(place_name, cc_tld)
+        places << {:name => place_name, :lat => position.lat, :lon => position.lon}
+      rescue => e
+        raise GadventuresParseError.new("error geocoding place name #{place_name}, region #{cc_tld}", e)
+      end
     end
 
     TripWriter.perform_async({:url => uri.to_s, :name => trip_name, :places => places})
 
   end
 
-  def get_places_from_itinerary(line)
+  def get_places_from_itinerary_line(line)
+    begin
       match = /^\s*Days?\s+\d+(\-\d+)?\s+(?<places>(\w|\p{Word}|\s)*)\s*/.match(line)
       places = match[:places].split('/')
       return places
+    rescue => e
+      raise GadventuresParseError.new("error parsing itinerary line #{line} for place name", e)
+    end
   end
 
   def get_cc_tld(countries)
