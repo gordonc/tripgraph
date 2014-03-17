d3ff91fa0283beba0f87e96f20c45ef8fb71ba91
gordonc <gchan@fastmail.fm>
2014-03-16 22:14:39 -0400

    filter out non-json response formats in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,9 @@
 class SearchController < ApplicationController
+  respond_to :json
+
   def index
     params.require(:q)
     @trip_places = Search.search(params[:q], params[:top_left], params[:bottom_right])
+    respond_with @trip_places
   end
 end
