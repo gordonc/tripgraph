5c8303cfed9246c3b94671defb7855ec85dae528
gordonc <gchan@fastmail.fm>
2014-03-16 21:36:35 -0400

    add maps controller for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 require 'sidekiq/web'
 
 Tripgraph::Application.routes.draw do
+  get "maps/index"
+  root "maps#index"
+
   get '/search', to: 'search#index'
   resources :trips
   resources :places
