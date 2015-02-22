93daf9480b73757cf6344873a39d936ae738ffcd
Gordon Chan <gordonc@Gordons-MacBook-Pro.local>
2015-02-22 16:41:29 -0500

    check for nil in elasticsearch geo_point conversion

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,18 +4,28 @@ class Place < ActiveRecord::Base
   def from_elasticsearch(place)
     self.id = place['id']
     self.name = place['name']
-    self.lat = place['location']['lat']
-    self.lon = place['location']['lon']
+    if not place['location'].nil?
+      self.lat = place['location']['lat']
+      self.lon = place['location']['lon']
+    end
   end
 
   def to_elasticsearch
     {
       id: self.id,
       name: self.name,
-      location: {
+      location: self.location_to_elasticsearch
+    }
+  end
+
+  def location_to_elasticsearch
+    if self.lat.nil? or self.lon.nil?
+      nil
+    else
+      {
         lat: self.lat,
         lon: self.lon
       }
-    }
+    end
   end
 end
